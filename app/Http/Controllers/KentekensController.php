6d22409386b26415e24c841479a87aa5d2d7ebee
Dylan Bos <dylanbos1996@gmail.com>
2017-10-26 10:06:07 +0200

    changed step1 to step2

--- app/Http/Controllers/KentekensController.php
+++ app/Http/Controllers/KentekensController.php
@@ -23,6 +23,6 @@ class KentekensController extends Controller
         $carroseriedata = json_decode(file_get_contents('https://opendata.rdw.nl/resource/vezc-m2t6.json?$limit=5000&$$app_token=R71sAZV5Xbq0c7WG1lD5Zmn0t&kenteken=' . $kenteken));
         $carroseriedata = $carroseriedata[0];
 
-        return view('kentekens.step1', compact('data', 'brandstof_data', 'carroseriedata'));
+        return view('kentekens.step2', compact('data', 'brandstof_data', 'carroseriedata'));
     }
 }
