18c34d4a2b0d7c455b9810a74f184ac86eb8ae8e
Dylan Bos <dylanbos1996@gmail.com>
2017-11-16 15:24:01 +0100

    minor css changes

--- resources/views/kentekens/step4.blade.php
+++ resources/views/kentekens/step4.blade.php
@@ -9,30 +9,21 @@
         <div class="info info-step4">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-3"></div><div class="col-sm-6">
-                        <div class="row">
+                    <div class="col-sm-3 hidden-sm"></div><div class="col-sm-12 col-md-6">
                             <h1>Gefeliciteerd! Je aanmelding is nu compleet</h1>
                             <p>
                                 Je ontvangt nu een email ter bevestiging. Hierin wordt een link vermeld.
                                 Door op de link te klikken activeer je de aanmelding.
                                 Wil je de informatie nog aanvullen of foto's toevoegen? Dit kan via de mail.
                             </p>
-                        </div>
-                    </div>
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('img/kentekens/kqKs2PMOcr0bW1fMrR14cFCyKRX8MU.png')}}" alt="">
-                        {{-- <img src="{{asset('img/pose2.png')}}" alt=""> --}}
                     </div>
                 </div>
-                <div class="row">
-                    @include('partials.steps', [
-                        'numofsteps' => 4,
-                        'active' => 3,
-                        'width' => "12 col-md-6",
-                        'offset' => "3 hidden-xs"
-                    ])
-                </div>
+                @include('partials.steps', [
+                    'numofsteps' => 4,
+                    'active' => 3,
+                    'width' => "12 col-md-6",
+                    'offset' => "3 hidden-xs"
+                ])
             </div>
         </div>
     </div>
