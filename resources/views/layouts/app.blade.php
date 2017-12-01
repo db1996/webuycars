a72d14a6359fa02112f551495553a22b2abed543
Dylan Bos <dylanbos1996@gmail.com>
2017-12-01 16:28:49 +0100

    Added favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,52 @@
 <!DOCTYPE html>
-{{-- @php
-Auth::logout();
-
-
-@endphp --}}
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" dir="ltr">
 <head>
+    <!-- Meta tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'WeBuyCars') }}</title>
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Font Awesome -->
 
-    <script src="https://use.fontawesome.com/ad9e1c6f38.js"></script>
-    <!-- JQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
-    <!-- Filinput scripts and stylesheets -->
+    <!-- SEO -->
+    <title>{{ config('app.name', 'WeBuyCars') }}</title>
+    <meta name="description" content="Description of the page less than 150 characters">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('img/favicon')}}/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{asset('img/favicon')}}/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('img/favicon')}}/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/favicon')}}/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('img/favicon')}}/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('img/favicon')}}/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('img/favicon')}}/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('img/favicon')}}/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/favicon')}}/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('img/favicon')}}/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicon')}}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('img/favicon')}}/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicon')}}/favicon-16x16.png">
+    <link rel="manifest" href="{{asset('img/favicon')}}/manifest.json">
+    <meta name="msapplication-config" content="{{asset('img/favicon')}}/browserconfig.xml">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{asset('img/favicon')}}/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <!-- bootstrap -->
+    <!-- Fileinput -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.5/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- Custom CSS -->
+
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!-- JQuery -->
+    <script src="{{asset('js/nl.js')}}"></script> <!-- Language script for fileinput-->
+    <script src="https://use.fontawesome.com/ad9e1c6f38.js"></script> <!-- Font Awesome -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <!-- Bootstrap -->
+    <!-- Filinput scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.5/js/plugins/sortable.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.5/js/plugins/purify.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.5/js/fileinput.min.js"></script>
-    <script src="{{asset('js/nl.js')}}"></script>
-    <!-- Custom CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script type="text/javascript" src="{{ asset('js/hammer.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hammer.js') }}"></script> <!-- Hammer (touch) -->
 </head>
 <body id="@yield('pagetype')">
     <div id="app">
