de2702045cf499998e88f053bcbf377bf2402fdf
Dylan Bos <dylanbos1996@gmail.com>
2017-12-05 10:55:03 +0100

    Added gooey loader

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,21 +1,21 @@
 <div class="preloader js-preloader-loaded">
-    <div class="c-loading c-loading--square-wave">
+    <div class="c-loading--gooey">
+        <div class="c-loading__gooey-dot c-loading__gooey-dot--dot1"></div>
+        <div class="c-loading__gooey-dot c-loading__gooey-dot--dot2"></div>
+        <div class="c-loading__gooey-dot c-loading__gooey-dot--dot3"></div>
+        {{-- <span class="c-loading__square-wave"></span>
         <span class="c-loading__square-wave"></span>
-        <span class="c-loading__square-wave"></span>
-        <span class="c-loading__square-wave"></span>
-        <span class="c-loading__square-wave"></span>
-        <span class="c-loading__square-wave"></span>
+        <span class="c-loading__square-wave"></span --}}
         {{-- <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
-        <span class="c-loading__wave"></span>
         <span class="c-loading__wave"></span>
         <span class="c-loading__wave"></span> --}}
     </div>
 </div>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <defs>
+        <filter id="gooey-filter">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"/>
+        </filter>
+    </defs>
+</svg>
