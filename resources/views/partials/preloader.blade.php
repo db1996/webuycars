b0ee7128ba66069c7587f2a2afda1e913f2d73c5
Dylan Bos <dylanbos1996@gmail.com>
2017-12-05 16:57:10 +0100

    google thing

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,13 +1,16 @@
 <div class="preloader js-preloader-loaded">
-    <div class="c-loading--google-spinner --fuck">
-        <div class="c-loading__spinner-single c-loading__spinner-single--right">
-            <div class="c-loading__spinner-inner c-loading__spinner-inner--right"></div>
-        </div>
-        <div class="c-loading__spinner-single c-loading__spinner-single--left">
-            <div class="c-loading__spinner-inner c-loading__spinner-inner--left"></div>
-        </div>
+    <div class="c-loading c-loading--google-circular">
+        <svg class="c-loading__circle-svg" viewBox="25 25 50 50">
+            <circle class="c-loading__circle-stroke" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+        </svg>
     </div>
 </div>
+
+    <div class="loader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+        </svg>
+    </div>
 
 {{-- Only use this if you want to use the Gooey --}}
 {{-- <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
