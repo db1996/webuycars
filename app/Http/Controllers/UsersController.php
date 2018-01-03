364a9f24f1571c55ee6a3a112100dca6dce41800
Dylan Bos <dylanbos1996@gmail.com>
2018-01-03 15:37:10 +0100

    daily commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     public function createPass(Request $request){
-        // dd($request->token);
-        return view('users.create-password');
+        return view('users.create-password', compact('request'));
     }
 }
--- resources/views/users/create-password.blade.php
+++ resources/views/users/create-password.blade.php
@@ -2,7 +2,6 @@
 @section('pagetype', 'static-nav')
 @section('content')
     <div class="c-card c-card--small">
-        <h1 class="c-card__title c-card__title--small">Maak een wachtwoord aan</h1>
-        <h2 class="c-card__subtitle c-card__subtitle--small"></h2>
+        <h1 class="c-card__title c-card__title--small">Maak een wachtwoord aan {{$request->token}}</h1>
     </div>
 @endsection
